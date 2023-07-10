35931c099221537924c1d40a0056c1fc6bb11a82
renata-aline <renatalinec@gmail.com>
2023-07-10 15:40:34 -0300

    melhorando codigo com async Await

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-      throw Error('Esse cep não existe!')  
-    }else
-    console.log(r)})
-.catch(erro => console.log(erro));
 
-console.log(consultaCEP);
+async function buscaEndereco(){
+var consultaCEP = await fetch('https://viacep.com.br/ws/05788300/json/')
+var consultaCEPConvertida = await consultaCEP.json();
+
+console.log(consultaCEPConvertida);
+
+}
+buscaEndereco();
