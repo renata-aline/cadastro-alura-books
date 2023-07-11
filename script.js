9cac72e719dda2cbea803110590c0fa19ebf6b8f
renata-aline <renatalinec@gmail.com>
2023-07-11 17:25:57 -0300

    preenchimento automático

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ async function buscaEndereco(cep) {
       throw Error('cep não existente!');
     }
 
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+    var bairro = document.getElementById('bairro');
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    bairro.value = consultaCEPConvertida.bairro;
+
+
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida;
   } catch (erro) {
@@ -14,8 +25,11 @@ async function buscaEndereco(cep) {
   }
 }
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
 
-buscaEndereco();
+// buscaEndereco();
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout",() => buscaEndereco(cep.value));
